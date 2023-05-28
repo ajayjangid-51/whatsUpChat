28bbbd259c1edcad134b0b8b150ddf6e8c092646
ajaykumar <csefeel@gmail.com>
2023-05-28 16:41:11 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -76,6 +76,9 @@ app.get("/", (req, res) => {
 	res.send("hi welcome...");
 });
 
+app.get("/api", (req, res) => {
+	res.send("hi i am h");
+});
 app.get("/api/h", (req, res) => {
 	res.send("hi i am h");
 });
